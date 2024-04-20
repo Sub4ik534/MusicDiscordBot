4fa08f0ee28409cd2a0cfd3d041ee517afa92fb1
Sasha <cacaanfalov@gmail.com>
2024-04-20 14:38:27 +0300

    added the ability to play tracks from Spotify

--- func.py
+++ func.py
@@ -2,12 +2,17 @@ import discord
 import os
 import asyncio
 import yt_dlp
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 
 
 def run_bot():
     load_dotenv()
     TOKEN = os.getenv('discord_token')
+    client_id_ = os.getenv('YOUR_CLIENT_ID')
+    client_secret_ = os.getenv('YOUR_CLIENT_SECRET')
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id_, client_secret=client_secret_))
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
@@ -33,7 +38,7 @@ def run_bot():
 
     @client.event
     async def on_ready():
-        print(f'{client.user} is now jamming')
+        print(f'{client.user} готов к работе')
 
     @client.event
     async def on_message(message):
@@ -64,6 +69,32 @@ def run_bot():
                     play_next_song(message.guild.id)
             except Exception as e:
                 print(e)
+
+        if "open.spotify.com/track/" in message.content:
+            track_id = message.content.split('track/')[1].split('?')[0]
+            try:
+                track_info = sp.track(track_id)
+                track_name = track_info['name']
+                artist_name = track_info['artists'][0]['name']
+                search_query = f"{track_name} {artist_name} audio"
+
+                loop = asyncio.get_event_loop()
+                results = await loop.run_in_executor(None, lambda: ytdl.extract_info(f"ytsearch:{search_query}",
+                                                                                     download=False))
+                video_url = results['entries'][0]['url']
+
+                song = {'url': video_url}
+                queue_song(message.guild.id, song)
+                if not voice_clients[message.guild.id].is_playing():
+                    play_next_song(message.guild.id)
+
+                player = discord.FFmpegOpusAudio(video_url, **ffmpeg_options)
+                voice_clients[message.guild.id].play(player)
+
+                response = f"Играет: {track_name} от {artist_name}"
+            except Exception as e:
+                response = f"Проблемы при обработке ссылки из Spotify: {e}"
+            await message.channel.send(response)
 
         if message.content.startswith("/pause"):
             try:
